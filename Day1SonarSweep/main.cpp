1ea9cc5b835e8fd428f43f5c9d3c04653093266a
Micah Weston <msweston12@cox.net>
2021-12-01 22:30:48 -0500

    Adds solution to day one of the competition.

---
+++ Day1SonarSweep/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+#define PART 2
+
+int main(int argc, const char *argv[]) {
+  std::fstream File;
+  if (argc > 1)
+    File.open(argv[1]);
+
+  std::istream &Input = argc == 1 ? std::cin : File;
+
+  std::vector<int> Sonar;
+
+  int N;
+  while (Input >> N)
+    Sonar.push_back(N);
+
+#if PART == 2
+  for (int i = 0; i < Sonar.size() - 2; ++i)
+    Sonar[i] = Sonar[i] + Sonar[i + 1] + Sonar[i + 2];
+#endif
+
+  int Result = std::inner_product(std::cbegin(Sonar),
+                                  std::next(std::cend(Sonar), -3),
+                                  std::next(std::cbegin(Sonar)),
+                                  int{0},
+                                  std::plus<int>{},
+                                  [](int L, int R) {
+                                    return L < R ? 1 : 0;
+                                  });
+
+  std::cout << Result << "\n";
+
+  return 0;
+}
