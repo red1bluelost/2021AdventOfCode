bb9ee0a365e1c48712f29f347ea74dbef1bf62ec
Micah Weston <msweston12@cox.net>
2021-12-02 20:19:46 -0500

    Changes since I'm nit picky.

--- Day1SonarSweep/main.cpp
+++ Day1SonarSweep/main.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 
 #ifndef PART
-#define PART 2
+#define PART 1
 #endif
 
 int main(int argc, const char *argv[]) {
@@ -22,20 +22,29 @@ int main(int argc, const char *argv[]) {
   while (Input >> N)
     Sonar.push_back(N);
 
-#if PART == 2
-  for (int i = 0; i < Sonar.size() - 2; ++i)
-    Sonar[i] = Sonar[i] + Sonar[i + 1] + Sonar[i + 2];
-#endif
-
-  constexpr auto BackShift = PART == 1 ? -1 : -3;
+#if PART == 1
   int Result = std::inner_product(std::cbegin(Sonar),
-                                  std::next(std::cend(Sonar), BackShift),
+                                  std::next(std::cend(Sonar), -1),
                                   std::next(std::cbegin(Sonar)),
                                   int{0},
                                   std::plus<int>{},
                                   [](int L, int R) {
                                     return L < R ? 1 : 0;
                                   });
+
+#elif PART == 2
+  for (int i = 0; i < Sonar.size() - 2; ++i)
+    Sonar[i] = Sonar[i] + Sonar[i + 1] + Sonar[i + 2];
+
+  int Result = std::inner_product(std::cbegin(Sonar),
+                                  std::next(std::cend(Sonar), -3),
+                                  std::next(std::cbegin(Sonar)),
+                                  int{0},
+                                  std::plus<int>{},
+                                  [](int L, int R) {
+                                    return L < R ? 1 : 0;
+                                  });
+#endif
 
   std::cout << Result << "\n";
 
